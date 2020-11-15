cd6c36a29f55d53a318dde9b0d608df1ebb3bf95
hchiam <hchiam@users.noreply.github.com>
2020-11-15 17:20:37 -0500

    make script easier to use

--- get-integrity.sh
+++ get-integrity.sh
@@ -5,3 +5,11 @@ function sha() {
 
 # Usage:
 # source get-integrity.sh; sha someFileName.js
+
+echo
+echo makeElementDraggable.js
+sha makeElementDraggable.js
+echo
+echo makeElementDraggableAndEditable.js
+sha makeElementDraggableAndEditable.js
+echo
