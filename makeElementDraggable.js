d02d5afe11ccd019b18d31ab926cdeb339b782e8
hchiam <hchiam@users.noreply.github.com>
2021-08-29 10:21:35 -0400

    ES5 auto-checker set up to catch more compatibility/accessibility bugs automatically

--- makeElementDraggable.js
+++ makeElementDraggable.js
@@ -143,12 +143,14 @@ function makeElementDraggable(element, settings) {
     }
   }
 
-  function snapToGrid(value, gridSize = 25) {
+  function snapToGrid(value, gridSize) {
+    gridSize = gridSize || 25;
     var newValue = gridSize * Math.floor(value / gridSize);
     return newValue;
   }
 
-  function isSnapPointInRange(snapPoint, left, top, threshold = 50) {
+  function isSnapPointInRange(snapPoint, left, top, threshold) {
+    threshold = threshold || 50;
     var a = snapPoint.x - left;
     var b = snapPoint.y - top;
     var c = Math.sqrt(a * a + b * b);
